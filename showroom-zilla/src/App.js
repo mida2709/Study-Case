1e7fc2f910e2af919e235f6dba37161ea6f0c7f1
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-05-18 03:25:50 +0700

    App.js

--- showroom-zilla/src/App.js
+++ showroom-zilla/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import Showroom from './Components/Showroom';
+import Frame from './images/Frame 27.png'
+import React from 'react';
+import Car from './Components/Car';
+
+
 function App() {
   return (
-    <div className="App">
-      <Showroom />
+    <div>
+      <header>
+        <img src={Frame} className="uk" alt="Frame" />
+        <h1 className="judul">Zilla Showroom</h1>
+        <div className="card">
+          <p className="cari">Cari Mobil Impian Anda</p>
+          <div className="search">
+            <p className="s">Search</p>
+          </div>
+          <div className="cardname">
+          </div>
+        </div>
+        <Car/>
+      </header>
+  
     </div>
   );
-}
+};
 
 export default App;
+
+
