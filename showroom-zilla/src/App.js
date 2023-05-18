5381ee2e88ba6977f81f199e48ec21b46167b6f4
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-05-19 01:46:37 +0700

    App.js

--- showroom-zilla/src/App.js
+++ showroom-zilla/src/App.js
@@ -1,29 +1,43 @@
-import Frame from './images/Frame 27.png'
-import React from 'react';
-import Car from './Components/Car';
-
+import React, { useState } from 'react';
+import { mobil } from './Components/Car';
+import logo from './images/logo.png';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredCars = mobil.filter(car =>
+    car.nama.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div>
-      <header>
-        <img src={Frame} className="uk" alt="Frame" />
-        <h1 className="judul">Zilla Showroom</h1>
-        <div className="card">
-          <p className="cari">Cari Mobil Impian Anda</p>
-          <div className="search">
-            <p className="s">Search</p>
+    <div className="App">
+      <div className="header">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <h2 className="search-text">Cari Mobil Impian Anda</h2>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+        className="search-input"
+      />
+      <div className="car-container">
+        {filteredCars.map((car) => (
+          <div key={car.id} className="car-card">
+            <img src={require(`./images/${car.path}`).default} alt={car.nama} className="car-image" />
+            <div className="car-details">
+              <h3>{car.nama}</h3>
+              <p>Merek: {car.merek}</p>
+              <p>Warna: {car.warna}</p>
+              <p>Tipe: {car.tipe}</p>
+              <button>LIHAT DETAIL</button>
+            </div>
           </div>
-          <div className="cardname">
-          </div>
-        </div>
-        <Car/>
-      </header>
-  
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
-
-
