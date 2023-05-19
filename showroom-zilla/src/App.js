6a3f756c99e11e891344ed9a1b58244beef0931c
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-05-19 09:06:40 +0700

    App.js

--- showroom-zilla/src/App.js
+++ showroom-zilla/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import { mobil } from './Components/Car';
 import logo from './images/logo.png';
+import '../src/css/Card.css';
 
-function App() {
+
+const CarCard = ({ car }) => {
+  return (
+    <div className="car-card">
+      <img src={require(`./images/${car.path}`).default} alt={car.nama} className="car-image" />
+      <div className="car-details">
+        <h3>{car.nama}</h3>
+        <p>Merek: {car.merek}</p>
+        <p>Warna: {car.warna}</p>
+        <p>Tipe: {car.tipe}</p>
+        <button>LIHAT DETAIL</button>
+      </div>
+      
+    </div>
+  );
+};
+
+const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const filteredCars = mobil.filter(car =>
@@ -14,7 +32,7 @@ function App() {
       <div className="header">
         <img src={logo} alt="Logo" className="logo" />
       </div>
-      <h2 className="search-text">Cari Mobil Impian Anda</h2>
+      <h3 className="search-text">Cari Mobil Impian Anda</h3>
       <input
         type="text"
         placeholder="Search..."
@@ -23,19 +41,10 @@ function App() {
         className="search-input"
       />
       <div className="car-container">
-        {filteredCars.map((car) => (
-          <div key={car.id} className="car-card">
-            <img src={require(`./images/${car.path}`).default} alt={car.nama} className="car-image" />
-            <div className="car-details">
-              <h3>{car.nama}</h3>
-              <p>Merek: {car.merek}</p>
-              <p>Warna: {car.warna}</p>
-              <p>Tipe: {car.tipe}</p>
-              <button>LIHAT DETAIL</button>
-            </div>
-          </div>
-        ))}
-      </div>
+  {filteredCars.map((car) => (
+    <CarCard key={car.id} car={car} />
+  ))}
+</div>
     </div>
   );
 }
